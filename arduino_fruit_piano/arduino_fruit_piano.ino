b803ae4b9b7ed75315b982f3baf85be42c0160cf
bibishte <bibishte@gmail.com>
2018-10-03 12:25:03 +0300

    added working code without sound fromprocessing

---
+++ arduino_fruit_piano/arduino_fruit_piano.ino
@@ -0,0 +1,56 @@
+void setup() {
+  Serial.begin(9600);
+  pinMode(8, OUTPUT);
+  
+}
+
+void loop() {
+  if(digitalRead(13)==HIGH)
+  {
+    while(true)
+    {
+      if (analogRead(A0) < 800)
+      {
+        tone(8, 523, 130);
+        //Serial.println( analogRead(A0));
+        delay(80);
+      }
+      else if (analogRead(A1) < 800)
+      {
+        tone(8, 587, 130);
+        //Serial.println( analogRead(A1));
+        delay(80);
+      }
+  
+      else if (analogRead(A2) < 800)
+      {
+        tone(8, 659, 130);
+        delay(80);
+      }
+      else if (analogRead(A3) < 800)
+      {
+        tone(8, 699, 130);
+        delay(80);
+      }
+      else if (analogRead(A4) < 800)
+      {
+        tone(8, 784, 130);
+        delay(80);
+      }
+      else if (analogRead(A5) < 800)
+      {
+      tone(8, 880, 130);
+      delay(80);
+      }
+    }
+
+  }
+  else
+  {
+
+    //kym processing za zwuk
+
+    Serial.println("hiiii");
+
+  }
+}
